a64e19eb1b7331cea76a98f22f641a1bec6cb52a
Cody Kurpanek <CodyKurpanek@gmail.com>
2021-11-15 21:13:11 -0800

    Edited piece header

--- header/pieces/piece.hpp
+++ header/pieces/piece.hpp
@@ -1,13 +1,21 @@
+#ifndef __piece_hpp__
+#define __piece_hpp__
+
+#include <string>
+
 class piece{
 private:
 	bool _color;
 	bool _icon;
 public:
 	bool getColor();
-	bool getIcon();
+	std::string getIcon();
 	void setColor(bool);
-	void setIcon(bool);
+	void setIcon(std::string);
 	virtual void move(startingSquare, endingSquare)=0;
 	virtual void moveCheck(startingSquare)=0;
-	virtual void capture(startingSquare, endingSquare)=0;	
+	virtual void capture(startingSquare, endingSquare)=0;
+};
+
+#endif
 
