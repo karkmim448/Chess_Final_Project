29edab9230ab4e100b18b8f361e3636df5ade81a
Cody Kurpanek <CodyKurpanek@gmail.com>
2021-11-16 12:24:01 -0800

    Working on abstract factories

---
+++ header/BlackPieceFactory.hpp
@@ -0,0 +1,15 @@
+#ifndef __BLACK_PIECE_FACTORY_HPP__
+#define __BLACK_PIECE_FACTORY_HPP__
+
+class BlackPieceFactory{
+    void DrawPawn();
+    void DrawRook();
+    void DrawKing();
+    void DrawKnight();
+    void DrawBishop()
+
+};
+
+
+
+#endif
