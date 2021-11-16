6fe5f2648e321a69ece6692d4f6ce7149fe297d0
Cody Kurpanek <CodyKurpanek@gmail.com>
2021-11-15 21:09:01 -0800

    created initial implementation of piece

---
+++ header/pieces/piece.cpp
@@ -0,0 +1,17 @@
+#include "piece.hpp"
+#include <string>
+
+
+bool piece::getColor(){
+	return this->_color;
+}
+std::string piece::getIcon(){
+	return this->_icon;
+}
+
+void piece::setColor(bool color){
+	this->_color = color;
+}
+void piece::setIcon(string icon){
+	this->_icon = icon
+}
