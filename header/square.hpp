fc42339cfedc43b4abfaa3c800eb1b3125d91252
DhillonSuhail <dhillonsuhail@gmail.com>
2021-11-21 22:24:54 -0800

    Game basic functions set up Game shouldnt need any more functions, everything else is handled in game

--- header/square.hpp
+++ header/square.hpp
@@ -1,18 +1,48 @@
-//#include </SFML/Graphics.hpp>
-#include <vector>
-#include <iostream>
+#ifndef __SQUARE_HPP__
+#define __SQUARE_HPP__
+
 #include "pieces/piece.hpp"
 
 class Square{
   private:
+    /* 
+      -True = White, False = Black
+      -Used by gui to determine square color, static after construction, never should be changed 
+    */
     bool _color;
-  public:
-    piece* piece;
-    piece* getPiece();
-    bool getCurrentColor();
-    bool getPieceColor();
-    void replacePiece(int newX, int newY);
-    void checkPiece();    //checks if piece taken is king->gameover
 
+    /*
+      -Used by gui, when a piece is selected, highlights the squares that the user can move that piece to
+      -Also used by game to determine if a move is valid, if the square is highlighted, the move is valid
+      -Should default to zero, having it set to one should be temporary
+      -Should be set to one only when a piece is selected but has not moved yet
+    */
+    bool _highlighted;
+    
+    /* 
+      -Strategy pattern, used for movement
+      -Used by Game to determine valid squares for a piece to move to
+      -Should be changed when a piece moves or a pawn promotes
+    */
+    piece* _piece;
+  public:
+    //Constructors
+    Square();
+    Square(bool color);
+    Square(bool color, piece* piece);
+
+    //Destructor
+    ~Square();
+
+    //Basic Accessors
+    bool getColor();
+    bool getHighlight();
+    piece* getPiece();
+    
+    //Basic Mutators
+    void setColor(bool color);//should never be used
+    void setHighlight(bool highlight);
+    void setPiece(piece* piece);
 };
 
+#endif //__SQUARE_HPP__//
