aa6a37672d376c9a35522918bd291da9fcf85912
Gurman Dhaliwal <gdhal005@ucr.edu>
2021-11-24 02:34:52 -0800

    main.cpp displaying chessboard with individually accesible objects, add image files

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,68 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Testing SFML");
+    sf::RenderWindow window(sf::VideoMode(900, 900), "Chess Dot Com");
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+  
+    sf::Texture lightSquare;
+    sf::Texture darkSquare;
+    lightSquare.loadFromFile("../src/pieces/images/light_square.png");  // "../src" needed to make cmake work for some reason idfk, doesnt work otherwise
+    darkSquare.loadFromFile("../src/pieces/images/dark_square.png");
 
     while(window.isOpen()){
-	sf::Event event;
-
-	while(window.pollEvent(event)){
+	    sf::Event event;
+    
+	    while(window.pollEvent(event)){
             if(event.type == sf::Event::Closed){
-                window.close();
+                    window.close();
+                }
+        }
+
+
+        sf::Sprite board[8][8];
+        
+        window.clear();
+        
+
+
+        for(int i = 0; i<8; ++i){
+
+            for (int j = 0; j < 8; ++j){
+                board[i][j].setTexture(lightSquare);
+                board[i][j].setPosition(100*i, 100*j);
+                window.draw(board[i][j]);
+                //window.display();
+                ++j;
+                board[i][j].setTexture(darkSquare);
+                board[i][j].setPosition(100*i, 100*j);
+                window.draw(board[i][j]);
+                //window.display();
+            }
+            ++i;
+            for (int j = 0; j < 8; ++j){
+                board[i][j].setTexture(darkSquare);
+                board[i][j].setPosition(100*i, 100*j);
+                window.draw(board[i][j]);
+                //window.display();
+                ++j;
+                board[i][j].setTexture(lightSquare);
+                board[i][j].setPosition(100*i, 100*j);
+                window.draw(board[i][j]);
+                //window.display();
             }
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+
+/*
+            lightSquare.loadFromFile("pieces/images/light_square.png");
+            sf::Sprite firstSquare;
+            firstSquare.setTexture(lightSquare);
+*/
+
+            
+            //window.draw(firstSquare);
+            window.display();
+        }
 
     return 0;
 }
