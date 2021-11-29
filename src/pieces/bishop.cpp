c0acdbc780efab51abc5676d40ce831d7fd76f6e
DhillonSuhail <dhillonsuhail@gmail.com>
2021-11-29 13:37:35 -0800

    Temp Commit

--- src/pieces/bishop.cpp
+++ src/pieces/bishop.cpp
@@ -5,3 +5,16 @@ bishop::bishop(): piece() {}
 bishop::bishop(bool color, std::string icon, Game* game): piece(color, icon, game) {}
 
 bishop::~bishop() {}
+
+void bishop::updatePossibleMoves(std::pair<int, int> position){
+    //clear updatePossibleMoves
+    this->getPossibleMoves()->clear();
+    
+    //cover forward right loop
+
+    //cover forward left loop
+
+    //cover backward right loop
+
+    //cover forward left loop
+}
